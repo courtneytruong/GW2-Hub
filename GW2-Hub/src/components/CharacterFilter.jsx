f8ed9116198663faefec93fc1f4b0e16e5b74d6c
courtneytruong <courtneybtruong@gmail.com>
2025-05-12 17:17:00 -0400

    drop down text color correction

--- GW2-Hub/src/components/CharacterFilter.jsx
+++ GW2-Hub/src/components/CharacterFilter.jsx
@@ -15,7 +15,7 @@ export default function CharacterFilter({
         placeholder="Search by name"
         value={searchName}
         onChange={(e) => setSearchName(e.target.value)}
-        className="border p-2 rounded"
+        className="border p-2 rounded "
       />
 
       <select
@@ -23,12 +23,14 @@ export default function CharacterFilter({
         onChange={(e) => setFilterRace(e.target.value)}
         className="border p-2 rounded"
       >
-        <option value="">All Races</option>
-        <option value="Human">Human</option>
-        <option value="Norn">Norn</option>
-        <option value="Sylvari">Sylvari</option>
-        <option value="Asura">Asura</option>
-        <option value="Charr">Charr</option>
+        <div className="text-black">
+          <option value="">All Races</option>
+          <option value="Human">Human</option>
+          <option value="Norn">Norn</option>
+          <option value="Sylvari">Sylvari</option>
+          <option value="Asura">Asura</option>
+          <option value="Charr">Charr</option>
+        </div>
       </select>
 
       <select
@@ -36,16 +38,18 @@ export default function CharacterFilter({
         onChange={(e) => setFilterProfession(e.target.value)}
         className="border p-2 rounded"
       >
-        <option value="">All Professions</option>
-        <option value="Warrior">Warrior</option>
-        <option value="Guardian">Guardian</option>
-        <option value="Revenant">Revenant</option>
-        <option value="Engineer">Engineer</option>
-        <option value="Ranger">Ranger</option>
-        <option value="Thief">Thief</option>
-        <option value="Elementalist">Elementalist</option>
-        <option value="Necromancer">Necromancer</option>
-        <option value="Mesmer">Mesmer</option>
+        <div className="text-black">
+          <option value="">All Professions</option>
+          <option value="Warrior">Warrior</option>
+          <option value="Guardian">Guardian</option>
+          <option value="Revenant">Revenant</option>
+          <option value="Engineer">Engineer</option>
+          <option value="Ranger">Ranger</option>
+          <option value="Thief">Thief</option>
+          <option value="Elementalist">Elementalist</option>
+          <option value="Necromancer">Necromancer</option>
+          <option value="Mesmer">Mesmer</option>
+        </div>
       </select>
 
       <select
@@ -53,16 +57,18 @@ export default function CharacterFilter({
         onChange={(e) => setFilterCrafting(e.target.value)}
         className="border p-2 rounded"
       >
-        <option value="">All Crafting</option>
-        <option value="Artificer">Artificer</option>
-        <option value="Armorsmith">Armorsmith</option>
-        <option value="Chef">Chef</option>
-        <option value="Huntsman">Huntsman</option>
-        <option value="Jeweler">Jeweler</option>
-        <option value="Leatherworker">Leatherworker</option>
-        <option value="Scribe">Scribe</option>
-        <option value="Tailor">Tailor</option>
-        <option value="Weaponsmith">Weaponsmith</option>
+        <div className="text-black">
+          <option value="">All Crafting</option>
+          <option value="Artificer">Artificer</option>
+          <option value="Armorsmith">Armorsmith</option>
+          <option value="Chef">Chef</option>
+          <option value="Huntsman">Huntsman</option>
+          <option value="Jeweler">Jeweler</option>
+          <option value="Leatherworker">Leatherworker</option>
+          <option value="Scribe">Scribe</option>
+          <option value="Tailor">Tailor</option>
+          <option value="Weaponsmith">Weaponsmith</option>
+        </div>
       </select>
     </div>
   );
