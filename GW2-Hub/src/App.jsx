91b212484704610ab1f9f3f27e4a9c0880a347fa
courtneytruong <courtneybtruong@gmail.com>
2025-05-21 15:58:55 -0400

    Navbar functional

--- GW2-Hub/src/App.jsx
+++ GW2-Hub/src/App.jsx
@@ -1,15 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CharacterList from "./pages/CharacterList";
+import Layout from "./components/Layout";
 
-export default function App() {
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="CharacterList" element={<CharacterList />} />
-        {/* future: <Route path="/character/:name" element={<CharacterDetail />} /> */}
+        {/* Every route below shares the Layout (and Navbar!) */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="characters" element={<CharacterList />} />
+          {/* future: <Route path="/character/:name" element={<CharacterDetail />} /> */}
+        </Route>
       </Routes>
     </Router>
   );
 }
+
+export default App;
+
+// export default function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="CharacterList" element={<CharacterList />} />
+//         {/* future: <Route path="/character/:name" element={<CharacterDetail />} /> */}
+//       </Routes>
+//     </Router>
+//   );
+// }
