2074b16e2a9293bd98b599cdbe92b87e01bec113
courtneytruong <courtneybtruong@gmail.com>
2025-05-28 15:01:59 -0400

    Header logo and changes to nav bar to fit the new logo

--- GW2-Hub/src/components/Navbar.jsx
+++ GW2-Hub/src/components/Navbar.jsx
@@ -3,57 +3,25 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      {/* Primary Navbar */}
-      <nav className="bg-gradient-to-t from-black to-red-800 shadow-md">
-        <div className="mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between items-center h-16">
-            {/* Logo */}
-            <div className="flex-shrink-0">GW2 Hub</div>
-
-            {/* Primary Links (hidden on mobile) */}
-            <div className="hidden md:flex space-x-6">
-              <a
-                href="/"
-                className="text-white hover:text-neutral-400 transition-colors"
-              >
-                Home
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-neutral-400 transition-colors"
-              >
-                About
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-neutral-400 transition-colors"
-              >
-                Contact
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
-
       {/* Secondary Bar */}
-      <div className="bg-white">
+      <div className="bg-gradient-to-t from-black to-red-800 shadow-md">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-center items-center h-10 space-x-4 text-sm">
             <a
               href="characters"
-              className="text-black hover:text-neutral-600 transition-colors"
+              className="text-white hover:text-neutral-300 transition-colors"
             >
               Characters
             </a>
             <a
               href="wizardvault"
-              className="text-black hover:text-neutral-600 transition-colors"
+              className="text-white  hover:text-neutral-300 transition-colors"
             >
               Wizard Vault
             </a>
             <a
               href="#"
-              className="text-black hover:text-neutral-600 transition-colors"
+              className="text-white hover:text-neutral-300 transition-colors"
             >
               Weeklies
             </a>
